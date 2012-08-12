d911d5d7cd27bd47fa52d8767d45d96fd4690f30
Anschel Schaffer-Cohen <anschelsc@gmail.com>
2012-08-12 15:31:38 -0400

    Docs

--- doscolor.go
+++ doscolor.go
@@ -7,8 +7,13 @@ import (
 	"syscall"
 )
 
+// A color is a color. The names are the same as the ANSI colors, but the
+// actual hues are pretty different. Wikipedia has a nice chart on the "ANSI
+// Colors" page.
 type Color uint16
 
+// These are the foreground colors; use BG() to get the equivalent background
+// colors.
 const (
 	Blue Color = 1 << iota
 	Green
@@ -22,24 +27,28 @@ const (
 	Black   = 0
 )
 
+// BG turns a foreground color into a background color.
 func BG(c Color) Color { return c << 4 }
 
-// Some useful masks
+// Some useful masks for use with SetMask.
 const (
 	Foreground = 0x0F
 	Background = 0xF0
 )
 
+// A wrapper lets you change the colors of text written to a DOS console.
 type Wrapper struct {
 	*os.File
 	h     w32.HANDLE
 	saved *w32.CONSOLE_SCREEN_BUFFER_INFO
 }
 
+// Don't call this unless f is a console (i.e. os.Stdout).
 func NewWrapper(f *os.File) *Wrapper {
 	return &Wrapper{f, w32.HANDLE(f.Fd()), nil}
 }
 
+// Save saves the current coloring so that it can be Restored later.
 func (w *Wrapper) Save() error {
 	w.saved = w32.GetConsoleScreenBufferInfo(w.h)
 	if w.saved == nil {
@@ -48,6 +57,8 @@ func (w *Wrapper) Save() error {
 	return nil
 }
 
+// Restore restores a previously Saved coloring. It is an error to call Restore
+// without ever having called Save.
 func (w *Wrapper) Restore() error {
 	if w.saved == nil {
 		return errors.New("attempted to restore without saving.")
@@ -58,11 +69,12 @@ func (w *Wrapper) Restore() error {
 	return nil
 }
 
-// Set c with the default mask (change both foreground and background)
+// Change the coloring to c.
 func (w *Wrapper) Set(c Color) error {
 	return w.SetMask(c, 0xFF)
 }
 
+// Change the coloring to c, but only in those bits where m is 1.
 func (w *Wrapper) SetMask(c Color, m uint16) error {
 	current := w32.GetConsoleScreenBufferInfo(w.h)
 	if current == nil {
